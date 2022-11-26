442c7d95bdfc3fc4fd07293cd49e16216c1589f2
Martin Lobger <martin@lobger.com>
2022-11-26 23:51:28 +0100

    Add blink example

---
+++ examples/led-blink/led-blink.ino
@@ -0,0 +1,32 @@
+//*******************************************************************************
+//  Description:  LEDDriver example that demonstrates blinking led.
+//  Author:       Martin Lobger
+//  Copyright:    Copyright (c) 2022 Martin Lobger
+//  License:      MIT License. See included LICENSE file.
+//*******************************************************************************
+#include "LEDDriver.h"
+
+// It is assumed that your board has a builtin LED connected to pin LED_BUILTIN.
+// If not, replace LED_BUILTIN with the correct pin number and/or connect an external LED.
+#define LED_PIN LED_BUILTIN
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Example 'led-blink' running");
+    Serial.println("LEDDriver will make led blink in the background.");
+    
+    // Attach led pin to channel 0
+    LED.attach(LED_PIN, 0);
+
+    // Initialize and start the LEDDriver
+    LED.begin();                
+    
+    // Blink led attached to channel 0 @ 0.5Hz
+    LED.blink(0, 0.5f);
+}
+
+void loop()
+{
+    // Nothing to do here - LEDDriver have created a task that runs in the background
+}
